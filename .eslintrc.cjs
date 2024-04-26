374e4f93bbc46d9f506e7158971438660d77ef32
Nguyễn Thành Đạt <thanhdat9096@gmail.com>
2024-04-26 14:55:42 +0700

    Add ESLint, Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -5,14 +8,55 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    // Disable các rule mà eslint xung đột với prettier.
+    // Để cái này ở dưới để nó override các rule phía trên!.
+    'eslint-config-prettier',
+    'prettier'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  plugins: ['react-refresh', 'prettier'],
+  settings: {
+    react: {
+      // Nói eslint-plugin-react tự động biết version của React.
+      version: 'detect'
+    },
+    // Nói ESLint cách xử lý các import
+    'import/resolver': {
+      node: {
+        paths: [path.resolve(__dirname, '')],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
   },
+  env: {
+    node: true
+  },
+  rules: {
+    'no-console': 1,
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    // Tắt rule yêu cầu import React trong file jsx
+    'react/react-in-jsx-scope': 'off',
+    // Cảnh báo khi thẻ <a target='_blank'> mà không có rel="noreferrer"
+    'react/jsx-no-target-blank': 'warn',
+    // Tăng cường một số rule prettier (copy từ file .prettierrc qua)
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        semi: false,
+        trailingComma: 'none',
+        tabWidth: 2,
+        endOfLine: 'auto',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true
+      }
+    ]
+  }
 }
