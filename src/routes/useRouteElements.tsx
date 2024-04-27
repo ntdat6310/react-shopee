8c97c04ed88358725356fdeb0fa3f7a69f456e91
Nguyễn Thành Đạt <thanhdat9096@gmail.com>
2024-04-27 22:26:28 +0700

    Add Header UI

--- src/routes/useRouteElements.tsx
+++ src/routes/useRouteElements.tsx
@@ -1,16 +1,16 @@
 import { useRoutes } from 'react-router-dom'
 import Footer from 'src/components/Footer'
 import RegisterHeader from 'src/components/RegisterHeader'
+import MainLayout from 'src/layouts/MainLayout'
 import RegisterLayout from 'src/layouts/RegisterLayout'
 import Login from 'src/pages/Login'
-import ProductList from 'src/pages/ProductList'
 import Register from 'src/pages/Register'
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
     {
       path: '/',
-      element: <ProductList />
+      element: <MainLayout />
     },
     {
       path: '/login',
