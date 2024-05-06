57ee6d1f52c18be2315b8f6547e9d103b4d3794a
Nguyễn Thành Đạt <thanhdat9096@gmail.com>
2024-05-06 13:52:00 +0700

    fix: remove variables declared but never used

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -15,7 +15,7 @@ export default class ErrorBoundary extends Component<Props, State> {
     this.state = { hasError: false }
   }
 
-  static getDerivedStateFromError(error: Error) {
+  static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI.
     return { hasError: true }
   }
